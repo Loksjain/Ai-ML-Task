328f30fb961e7ece6249d5eb9d2ca212217d997e
Saket <saketruia18@gmail.com>
2024-10-28 17:58:41 +0530

    changed data set for more accurate prediction

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,20 @@
+# Task:
+# Build a Regression Model for Taxi Trip Fare Prediction
+
+# You are provided with training and testing datasets to predict the fare of taxi trips.
+
+# Instructions:
+
+# Train the Model:
+
+# Use the training dataset to train your regression model.
+# Ensure you apply appropriate preprocessing techniques (data cleaning, normalization, feature selection, etc.).
+# Aim to achieve a 97-100% accuracy in fare prediction.
+
+# Test the Model:
+
+# After training, evaluate the model using the testing dataset.
+# Analyze the prediction accuracy and performance of your model, ensuring it reaches the desired accuracy range.
+# Objective:
+# The goal is to create a highly accurate model that predicts taxi trip fares with 97-100% accuracy based on the available features in the dataset.
+
